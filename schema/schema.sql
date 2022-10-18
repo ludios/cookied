50647990051840f7fdf224e970c8ad8d058eb89a
Ivan Kozik <ivan@ludios.org>
2022-10-18 04:26:39 +0000

    WIP schema

---
+++ schema/schema.sql
@@ -0,0 +1,17 @@
+\set ON_ERROR_STOP on
+
+CREATE SCHEMA cards;
+SET search_path TO cards;
+
+CREATE OR REPLACE FUNCTION raise_exception() RETURNS trigger AS $$
+DECLARE
+    message text;
+BEGIN
+    message := TG_ARGV[0];
+    RAISE EXCEPTION '%', message;
+END;
+$$ LANGUAGE plpgsql;
+
+-- A superuser should apply extensions.sql first
+
+\ir cards.sql
