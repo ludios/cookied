32149a48837c472554a741be4d1ade98c942af6f
Ivan Kozik <ivan@ludios.org>
2023-02-07 18:52:56 +0000

    schema/users: add visual_username to prevent the creation of similar-looking usernames

--- schema/users.sql
+++ schema/users.sql
@@ -1,8 +1,40 @@
+-- Restrict to valid DNS labels, just in case one needs per-user subdomains later
+-- Restrict to 31 characters instead of 63 because that is too many
+CREATE DOMAIN username AS text
+    CHECK (
+        octet_length(VALUE) >= 1 AND
+        octet_length(VALUE) <= 31 AND
+        -- A host name (label) MUST NOT start or end with a '-' (dash)
+        VALUE !~ '\A-' AND
+        VALUE !~ '-\Z' AND
+        -- A host name (label) MUST NOT consist of all numeric values
+        VALUE !~ '\A[0-9]+\Z' AND
+        VALUE ~ '\A[-a-zA-Z0-9]+\Z'
+    );
+
+-- Like username, but all lowercase and with [i1] -> l and 0 -> o to avoid visual ambiguity
+CREATE DOMAIN visual_username AS text
+    CHECK (
+        VALUE ~ '\A[-a-hj-z1-9]+\Z'
+    );
+
+CREATE FUNCTION get_visual_username(username username) RETURNS visual_username
+    LANGUAGE SQL
+    IMMUTABLE
+    RETURN
+        REPLACE(
+            REPLACE(
+                REPLACE(LOWER($1),
+                'i', 'l'),
+            '1', 'l'),
+        '0', 'o');
+
 CREATE TABLE users (
     -- Limit of 1T can be raised if needed
-    id             bigint       GENERATED ALWAYS AS IDENTITY PRIMARY KEY CHECK (id >= 1 AND id < 1000000000000),
-    username       text         NOT NULL,
-    creation_time  timestamptz  NOT NULL
+    id               bigint           GENERATED ALWAYS AS IDENTITY PRIMARY KEY CHECK (id >= 1 AND id < 1000000000000),
+    username         username         NOT NULL,
+    visual_username  visual_username  GENERATED ALWAYS AS (get_visual_username(username)) STORED,
+    creation_time    timestamptz      NOT NULL
 );
 -- After we've stabilized the table a bit
 --SELECT periods.add_system_time_period('users', 'row_start', 'row_end');
