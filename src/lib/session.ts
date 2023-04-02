60d9d0ea7e67271316e336ed27cecf844ffbc620
Ivan Kozik <ivan@ludios.org>
2023-04-02 12:53:20 +0000

    session: add more tests for SessionCookie; throw BadSessionCookieError instead of AssertionError in a case where the session_id has the allowed max number of digits but is still too high

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,4 +1,5 @@
 import { A } from "ayy";
+import { createHash } from "node:crypto";
 
 export class BadSessionCookieError extends Error {
 	constructor(message: string) {
@@ -6,14 +7,16 @@ export class BadSessionCookieError extends Error {
 	}
 }
 
+const MAX_SESSION_ID = 2n ** 63n - 1n;
+
 export class SessionCookie {
 	id: bigint;
 	secret: Buffer;
 
 	constructor(id: bigint, secret: Buffer) {
-		A.gte(id, 1n);
-		A.lte(id, 2n ** 63n - 1n);
-		A.eq(secret.length, 16);
+		A.gte(id, 1n, "id must be >= 1");
+		A.lte(id, MAX_SESSION_ID, `id must be <= ${MAX_SESSION_ID}`);
+		A.eq(secret.length, 16, "secret must be a 16-byte Buffer");
 		this.id = id;
 		this.secret = secret;
 	}
@@ -24,7 +27,7 @@ export class SessionCookie {
 		// len(str(2 ** 63 - 1)) = 19
 		// len("9223372036854775807") = 19
 		if (!/^[1-9][0-9]{0,18}$/.test(session_id_string)) {
-			throw new BadSessionCookieError("session_id was non-numeric or too long");
+			throw new BadSessionCookieError("session_id was invalid");
 		}
 		// >>> ("\x00" * 16).encode("base64")
 		// 'AAAAAAAAAAAAAAAAAAAAAA==\n'
@@ -33,15 +36,28 @@ export class SessionCookie {
 			throw new BadSessionCookieError("unpadded_base64_secret had wrong length or invalid characters");
 		}
 		const session_id = BigInt(session_id_string);
+		// Check before the constructor does, because we want BadSessionCookieError
+		if (session_id > MAX_SESSION_ID) {
+			throw new BadSessionCookieError("session_id was invalid");
+		}
 		const secret = Buffer.from(unpadded_base64_secret, "base64");
 		return new SessionCookie(session_id, secret);
+	}
+
+	hashedSecret(): Buffer {
+		const hash = createHash("sha384");
+		hash.update(this.secret);
+		const digest = hash.digest();
+		// We need just 128 of the 384 bits
+		const hashed_secret = digest.subarray(0, 16);
+		return hashed_secret;
 	}
 
 	toString() {
 		const base64_secret = this.secret.toString("base64");
 		A.eq(base64_secret.length, 24);
 		A(base64_secret.endsWith("=="));
-		const unpadded_base64_secret = base64_secret.replace(/==$/, "");
+		const unpadded_base64_secret = base64_secret.substring(0, 22);
 		return `${this.id} ${unpadded_base64_secret}`;
 	}
 }
--- tests/session_test.ts
+++ tests/session_test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import { SessionCookie } from "../src/lib/session.js";
+import { SessionCookie, BadSessionCookieError } from "../src/lib/session.js";
 import { AssertionError } from "ayy";
 
 const fn = () => 'foo';
@@ -10,4 +10,17 @@ test("SessionCookie constructor", t => {
 	t.throws(() => new SessionCookie(2n ** 64n, Buffer.alloc(16)), {instanceOf: AssertionError}); // id > 2 ** 63 - 1
 	t.throws(() => new SessionCookie(1n, Buffer.alloc(15)), {instanceOf: AssertionError}); // wrong secret length
 	t.throws(() => new SessionCookie(1n, Buffer.alloc(17)), {instanceOf: AssertionError}); // wrong secret length
+});
+
+test("SessionCookie parse", t => {
+	t.deepEqual(SessionCookie.parse("1 AAAAAAAAAAAAAAAAAAAAAA"), new SessionCookie(1n, Buffer.from("00000000000000000000000000000000", "hex")));
+	t.deepEqual(SessionCookie.parse("1 /+AAAAAAAAAAAAAAAAAAAA"), new SessionCookie(1n, Buffer.from("ffe00000000000000000000000000000", "hex")));
+	t.deepEqual(SessionCookie.parse("9223372036854775807 /+AAAAAAAAAAAAAAAAAAAA"), new SessionCookie(9223372036854775807n, Buffer.from("ffe00000000000000000000000000000", "hex")));
+	t.throws(() => SessionCookie.parse("92233720368547758070 AAAAAAAAAAAAAAAAAAAAAA"), {instanceOf: BadSessionCookieError, message: "session_id was invalid"});
+	t.throws(() => SessionCookie.parse("9223372036854775808 AAAAAAAAAAAAAAAAAAAAAA"), {instanceOf: BadSessionCookieError, message: "session_id was invalid"});
+	t.throws(() => SessionCookie.parse("0 AAAAAAAAAAAAAAAAAAAAAA"), {instanceOf: BadSessionCookieError, message: "session_id was invalid"});
+	t.throws(() => SessionCookie.parse("1 AAAAAAAAAAAAAAAAAAAAAAA"), {instanceOf: BadSessionCookieError, message: "unpadded_base64_secret had wrong length or invalid characters"});
+	t.throws(() => SessionCookie.parse("1 AAAAAAAAAAAAAAAAAAAAA"), {instanceOf: BadSessionCookieError, message: "unpadded_base64_secret had wrong length or invalid characters"});
+	t.throws(() => SessionCookie.parse("1 AAAAAAAAAA_AAAAAAAAAAA"), {instanceOf: BadSessionCookieError, message: "unpadded_base64_secret had wrong length or invalid characters"});
+	t.throws(() => SessionCookie.parse("1 AAAAAAAAAAAAAAAAAAAA=="), {instanceOf: BadSessionCookieError, message: "unpadded_base64_secret had wrong length or invalid characters"});
 });
