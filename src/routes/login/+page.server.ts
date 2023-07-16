1da50c54f3007115d457c9224809ca8c9132bab9
Ivan Kozik <ivan@ludios.org>
2023-07-16 02:38:55 +0000

    routes: fix TypeScript errors
    ```
    src/routes/login/+page.server.ts
      5:35  error  Unexpected any. Specify a different type  @typescript-eslint/no-explicit-any
      5:48  error  Unexpected any. Specify a different type  @typescript-eslint/no-explicit-any
    ```

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,7 +2,7 @@ import type { Actions } from "./$types";
 import { PrismaClient } from "@prisma/client";
 import { inspect } from "util";
 
-function throw_if_gt1(rows: Array<any>): Array<any> {
+function throw_if_gt1<T>(rows: Array<T>): Array<T> {
 	if (rows.length > 1) {
 		throw Error(`expected 0 or 1 rows, got ${rows.length} rows: ${inspect(rows)}`);
 	}
@@ -17,7 +17,7 @@ export const actions = {
 		const prisma = new PrismaClient();
 		// We have an index on LOWER(username) but not username
 		const users = throw_if_gt1(
-			await prisma.$queryRaw`SELECT * FROM cards.users WHERE LOWER(username) = ${username.toLowerCase()}`,
+			await prisma.$queryRaw`SELECT * FROM cards.users WHERE LOWER(username) = ${username.toLowerCase()}` satisfies Array<{ username: string }>,
 		);
 		if (users.length && users[0].username === username) {
 			console.log(`Found user! ${inspect(users[0])}`);
