31b3331aab676ed39a9a771e99469e50aa81fc3f
Ivan Kozik <ivan@ludios.org>
2023-07-24 06:14:19 +0000

    Show the session information if user provided a valid session

--- src/lib/db/session.ts
+++ src/lib/db/session.ts
@@ -5,6 +5,13 @@ import crypto from "node:crypto";
 
 const prisma = new PrismaClient();
 
+type MinimizedDatabaseSession = {
+	id: number,
+	user_id: number,
+	birth_time: Date,
+	user_agent_seen_first: string,
+};
+
 export class Session {
 	static async byId(session_id: bigint) {
 		return prisma.sessions.findUnique({where: { id: session_id }});
@@ -20,11 +27,19 @@ export class Session {
 		` satisfies Array<{ id: bigint, secret: Buffer }>);
 	}
 
-	static async isValid(session: SessionCookie): Promise<boolean> {
+	static async validate(session: SessionCookie): Promise<MinimizedDatabaseSession | null> {
 		const dbSession = await Session.byId(session.id);
 		if (!dbSession) {
-			return false;
+			return null;
 		}
-		return crypto.timingSafeEqual(session.hashedSecret(), dbSession.hashed_secret);
+		if (!crypto.timingSafeEqual(session.hashedSecret(), dbSession.hashed_secret)) {
+			return null;
+		}
+		return {
+			id: Number(dbSession.id),
+			user_id: Number(dbSession.user_id),
+			birth_time: dbSession.birth_time,
+			user_agent_seen_first: dbSession.user_agent_seen_first,
+		};
 	}
 }
