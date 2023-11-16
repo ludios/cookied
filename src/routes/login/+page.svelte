c315a41a1348b3b3729832d6a2724e41ffb8d3f7
Ivan Kozik <ivan@ludios.org>
2023-11-16 09:04:49 +0000

    When logging in or out, invalidate all data
    This prevents <Header /> from showing outdated session info.

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+	import { invalidateAll } from "$app/navigation";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		// Prevent Header from showing stale session data
+		invalidateAll();
+	});
+</script>
+
 Logged in?
