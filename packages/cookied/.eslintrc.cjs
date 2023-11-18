9b318c2a5fe04b4da41bb6061c4986f185ee40a5
Ivan Kozik <ivan@ludios.org>
2023-11-18 22:04:49 +0000

    WIP split into two packages: `cookied` library and `demo` SvelteKit site

--- packages/cookied/.eslintrc.cjs
+++ packages/cookied/.eslintrc.cjs
@@ -3,7 +3,6 @@ module.exports = {
 	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
-		"plugin:svelte/recommended"
 	],
 	rules: {
 		// https://eslint.org/blog/2022/07/interesting-bugs-caught-by-no-constant-binary-expression/
@@ -14,21 +13,10 @@ module.exports = {
 	parserOptions: {
 		sourceType: "module",
 		ecmaVersion: 2020,
-		extraFileExtensions: [".svelte"]
 	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
 	},
-	overrides: [
-		{
-			files: ["*.svelte"],
-			parser: "svelte-eslint-parser",
-			parserOptions: {
-				parser: "@typescript-eslint/parser"
-			}
-		}
-	],
-	ignorePatterns: ["/benchmarks/*"],
 };
