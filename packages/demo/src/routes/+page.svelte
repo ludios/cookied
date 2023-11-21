f84f09f969df7e4533c8acc5b4b9c1761731f9b9
Ivan Kozik <ivan@ludios.org>
2023-11-21 20:19:06 +0000

    demo: factor out LoginForm

--- packages/demo/src/routes/+page.svelte
+++ packages/demo/src/routes/+page.svelte
@@ -1,21 +1,11 @@
+<script>
+	import LoginForm from "./LoginForm.svelte";
+</script>
+
 <svelte:head>
 	<title>cookied demo site</title>
 </svelte:head>
 
 <h1>cookied demo site</h1>
 
-<form method="POST" action="/login">
-	<div class="container">
-		<label>
-			<span>Username</span><br>
-			<input id="username" name="username" type="text" autocomplete="username">
-		</label>
-        <br>
-
-		<label>
-			<span>Password</span><br>
-			<input id="password" name="password" type="password" autocomplete="current-password">
-		</label>
-	</div>
-	<button>Log in</button>
-</form>
+<LoginForm />
