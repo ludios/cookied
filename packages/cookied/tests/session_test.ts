794d7aa482275d315601d852d2cd7d18211f7232
Ivan Kozik <ivan@ludios.org>
2023-11-22 21:36:15 +0000

    Formatting

--- packages/cookied/tests/session_test.ts
+++ packages/cookied/tests/session_test.ts
@@ -4,10 +4,10 @@ import { BadSessionCookieError, SessionCookie } from "../lib/session.js";
 
 test("new SessionCookie", (t) => {
 	new SessionCookie(1n, Buffer.alloc(16));
-	t.throws(() => new SessionCookie(0n, Buffer.alloc(15)), { instanceOf: AssertionError }); // id < 1
+	t.throws(() => new SessionCookie(0n,        Buffer.alloc(15)), { instanceOf: AssertionError }); // id < 1
 	t.throws(() => new SessionCookie(2n ** 64n, Buffer.alloc(16)), { instanceOf: AssertionError }); // id > 2 ** 63 - 1
-	t.throws(() => new SessionCookie(1n, Buffer.alloc(15)), { instanceOf: AssertionError }); // wrong secret length
-	t.throws(() => new SessionCookie(1n, Buffer.alloc(17)), { instanceOf: AssertionError }); // wrong secret length
+	t.throws(() => new SessionCookie(1n,        Buffer.alloc(15)), { instanceOf: AssertionError }); // wrong secret length
+	t.throws(() => new SessionCookie(1n,        Buffer.alloc(17)), { instanceOf: AssertionError }); // wrong secret length
 });
 
 test("SessionCookie.parse", (t) => {
