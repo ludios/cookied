6e09de7511741d22470e79c90b205c091e6f8f97
Ivan Kozik <ivan@ludios.org>
2023-11-22 07:44:49 +0000

    Add dev/hash-password.mjs

---
+++ packages/cookied/dev/hash-password.mjs
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+// Reads a password from stdin and writes an argon2id hash to stdout
+
+import { argon2id, argon2Verify } from "hash-wasm";
+import fs from "node:fs";
+
+const STDIN_FILENO = 0;
+const stdin_buffer = fs.readFileSync(STDIN_FILENO);
+const password = stdin_buffer.toString();
+
+const salt = new Uint8Array(16);
+crypto.getRandomValues(salt);
+
+// https://www.rfc-editor.org/rfc/rfc9106.html#section-4-6.2
+const key = await argon2id({
+  password: password,
+  salt,
+  parallelism: 4,
+  iterations: 3,
+  memorySize: 64 * 1024,
+  hashLength: 32,
+  outputType: "encoded",
+});
+
+console.log(key);
