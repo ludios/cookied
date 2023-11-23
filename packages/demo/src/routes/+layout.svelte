87c516a78451152210e196123904bb927a8dd788
Ivan Kozik <ivan@ludios.org>
2023-11-23 17:47:53 +0000

    demo: add some minimal styling because browsers default to serif font

--- packages/demo/src/routes/+layout.svelte
+++ packages/demo/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { LayoutServerData } from './$types';
 	import Header from './Header.svelte';
+	import './styles.css';
 
 	export let data: LayoutServerData;
 </script>
