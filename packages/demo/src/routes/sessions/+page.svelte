76f9659de13e9dbf478217207234b6bb1e355d30
Ivan Kozik <ivan@ludios.org>
2023-11-26 23:27:08 +0000

    demo: sessions page: show "you are logged out" if logged out

--- packages/demo/src/routes/sessions/+page.svelte
+++ packages/demo/src/routes/sessions/+page.svelte
@@ -11,24 +11,28 @@
 <div class="text-column">
 	<h1>Sessions</h1>
 
-	<p>This lists all of the sessions on which you are logged in (username={data.session?.username}, id={data.session?.user_id}).</p>
+	{#if !data.session}
+		<p>You are logged out, so you have no user for which to show corresponding sessions.</p>
+	{:else}
+		<p>This lists all of the sessions on which you are logged in (username={data.session?.username}, id={data.session?.user_id}).</p>
 
-	<table border=1>
-		<thead>
-			<tr>
-				<th scope="col">ID</th>
-				<th scope="col">Time created</th>
-				<th scope="col">User agent when created</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each data.my_sessions as { id, birth_time, user_agent_seen_first } (id)}
-			<tr>
-				<td>{id}</td>
-				<td>{birth_time}</td>
-				<td>{user_agent_seen_first}</td>
-			</tr>
-			{/each}
-		</tbody>
-	</table>
+		<table border=1>
+			<thead>
+				<tr>
+					<th scope="col">ID</th>
+					<th scope="col">Time created</th>
+					<th scope="col">User agent when created</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each data.my_sessions as { id, birth_time, user_agent_seen_first } (id)}
+				<tr>
+					<td>{id}</td>
+					<td>{birth_time}</td>
+					<td>{user_agent_seen_first}</td>
+				</tr>
+				{/each}
+			</tbody>
+		</table>
+	{/if}
 </div>
