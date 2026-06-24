41e9c5d19de8c3a9682b0dbd51116b697fbaa911
Ivan Kozik <ivan@ludios.org>
2026-06-24 20:01:00 +0000

    demo/src/app.d.ts: import the type we use
    Slop-provider: ChatGPT 5.5 Thinking

--- packages/demo/src/app.d.ts
+++ packages/demo/src/app.d.ts
@@ -1,14 +1,11 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
+import type { MinimizedDatabaseSession } from "cookied/lib/db/session";
 
-// https://stackoverflow.com/questions/73738077/how-to-extend-locals-interface-in-sveltekit
-declare namespace App {
-	interface Locals {
-		session: MinimizedDatabaseSession | null;
+declare global {
+	namespace App {
+		interface Locals {
+			session: MinimizedDatabaseSession | null;
+		}
 	}
-
-	interface PageData {}
-
-	interface Platform {}
 }
+
+export {};
